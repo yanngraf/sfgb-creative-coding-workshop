891d491c0da91f5005be4acdaf256edd1884d14a
yanngraf <yann@yanngraf.com>
2019-08-12 22:48:20 +0200

    initial commit

--- examples/2-darts-color/sketch.js
+++ examples/2-darts-color/sketch.js
@@ -1,91 +1,86 @@
-let rounding = 1;
-let direction = 1;
 let scl = 115;
-
 let noiseVal;
 let counter = 0;
-
 let initializeFrame = 0;
 let colorSet = {};
 colorSet = ["white","#f4e11b","#1378b3","black"];
-let colorSetOrder = [0,1,2,3];
-
+let newColorSet = [];
+newColorSet[0] = [];
+let row ;
+let column;
 
 function setup() {
   createCanvas(800, 800);
+  background(255);
+  noFill();
+  noStroke();
+}
+
+function draw() {
+  column = 1;
+  
+  for (var x = 0; x < height; x = x+scl) {
+    
+    if (initializeFrame == 0) {
+      newColorSet[column] = [] ;
+    }
+    row = 1;
+    
+    for(var y = 0; y < width; y=y+scl){
+
+      if (initializeFrame == 0) {
+
+        newColorSet[column][row] = [] ;
+
+        let newarr = shuffle(colorSet);
+        colorSet = newarr;
+        newColorSet[column][row] = colorSet;
+      }
+
+      noiseVal = noise(x+counter,y);
+      noiseVal = map(noiseVal,0,1,0,scl);
+
+      center_x = x+noiseVal;
+      center_y = y+noiseVal;
+
+      print (newColorSet);
+      fill(getColor(column,row,0));
+      triangle(x,y,center_x,center_y,x,y+scl);
+
+      fill(getColor(column,row,1));
+      triangle(x,y,center_x,center_y,x+scl,y);
+
+      fill(getColor(column,row,2));
+      triangle(x,y+scl,center_x,center_y,x+scl,y+scl);
+
+      fill(getColor(column,row,3));
+      triangle(x+scl,y,center_x,center_y,x+scl,y+scl);
+
+      counter = counter + 0.0005;
+      row++;
+    }
+   column++;
+  }
+
+  if (initializeFrame == 0) {
+    initializeFrame = 1;
+  }
 }
 
 
-
-function draw() {
-  
-  background(0);
-  noFill(fill);
-  stroke(255);
-  //strokeWeight(2);
-  noStroke();
-  
-  
-  
-  for (var x = 0; x < height; x = x+scl) {
+function keyPressed() {
+  initializeFrame = 0;
+}
 
 
-  
-
-        for(var y = 0; y < width; y=y+scl){
-
-         
-                //console.log(colorSet);
-                if (initializeFrame == 0) {
-
-      
-                  print(colorSet);
-                  let newarr = shuffle(colorSet);
-  
-                  print(newarr);
-
-                  colorSet = newarr;
-                  //console.log(colorSet);
-                  //console.log(y);
-                }
-
-          noiseVal = noise(x+counter,y);
-          noiseVal = map(noiseVal,0,1,0,scl);
- 
-          center_x = x+noiseVal;
-          center_y = y+noiseVal;
-  
-          fill(colorSet[0]);
-          triangle(x,y,center_x,center_y,x,y+scl);
-
-          fill(colorSet[1]);
-          triangle(x,y,center_x,center_y,x+scl,y);
-
-          fill(colorSet[2]);
-          triangle(x,y+scl,center_x,center_y,x+scl,y+scl);
-
-
-          fill(colorSet[3]);
-          triangle(x+scl,y,center_x,center_y,x+scl,y+scl);
-
-          // line (x,y,center_x,center_y);
-          // line (center_x,center_y,x+scl,y);
-          // line (x,y+scl,center_x,center_y);
-          // line (center_x,center_y,x+scl,y+scl);
-
-          counter = counter + 0.0005;
-        
-
-                
-
-         
-        }   
-        
-        initializeFrame = 1;
-
+function getColor (column, row,id) {
+  if (newColorSet[column] == null) {
+    newColorSet[column] = [];
   }
-
-  
+  if (newColorSet[column][row] == null) {
+    newColorSet[column][row] = shuffle(colorSet)
+  }
+  return newColorSet[column][row][id];
 }
 
 function mouseMoved() {
@@ -93,10 +88,7 @@ function mouseMoved() {
   scl = map(mouseX,0,300,10,100);
 }
 
-/**
- * Randomize array element order in-place.
- * Using Durstenfeld shuffle algorithm.
- */
+
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
